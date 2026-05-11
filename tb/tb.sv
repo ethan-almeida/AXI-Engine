d899af725d15e0ac4f0acc8eecccefc7c4fe34f1
ethan-almeida <ethanalmeida184@gmail.com>
2026-05-11 16:22:40 -0600

    basic testbench/test for initial compile

---
+++ tb/tb.sv
@@ -0,0 +1,8 @@
+import uvm_pkg::*;
+`include "uvm_macros.svh"
+
+module tbench_top;
+    initial begin
+        run_test();
+    end
+endmodule
---
+++ tb/test.sv
@@ -0,0 +1,13 @@
+class my_test extends uvm_test;
+    `uvm_component_utils(my_test)
+
+    function new(string name = "my_test", uvm_component parent = null);
+        super.new(name, parent);
+    endfunction
+
+    virtual task run_phase(uvm_phase phase);
+        phase.raise_objection(this);
+        `uvm_info("MY_TEST", "hello from verilator + uvm!", UVM_LOW)
+        phase.drop_objection(this);
+    endtask
+endclass

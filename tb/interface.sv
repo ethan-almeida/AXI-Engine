a89c796ac4daaa0912536ca58942b486dc58843d
ethan-almeida <ethanalmeida184@gmail.com>
2026-05-12 23:06:00 -0600

    added an interface for both write and read logic

---
+++ tb/interface.sv
@@ -0,0 +1,45 @@
+interface axi_lite_intf #(DATA_WIDTH = 32, ADDR_WIDTH = 32)
+(
+    input logic clk
+);
+
+logic [ADDR_WIDTH-1:0] aw_addr;
+logic aw_valid, aw_ready;
+
+logic [DATA_WIDTH-1:0] w_data;
+logic [3:0] w_strb;
+logic w_valid, w_ready;
+
+logic [1:0] b_resp;
+logic b_valid, b_ready;
+
+logic [ADDR_WIDTH-1:0] r_data;
+logic [1:0] r_resp;
+logic r_valid;
+logic r_ready;
+
+logic [ADDR_WIDTH-1:0] ar_addr;
+logic                  ar_valid;
+logic                  ar_ready;
+
+logic reset, pulse;
+
+modport driver (output aw_addr, aw_valid, 
+                input aw_ready,
+                output w_data, w_strb, w_valid, 
+                input w_ready,
+                input b_resp, b_valid, 
+                output b_ready,
+                output reset
+                );
+
+modport monitor (input aw_addr, aw_valid, aw_ready, 
+                 input ar_addr, ar_ready, ar_valid, 
+                 input w_data, w_strb, w_valid, w_ready, 
+                 input b_resp, b_valid, b_ready, 
+                 input r_data, r_resp, r_valid, r_ready, 
+                 input reset, pulse
+                );
+
+
+endinterface

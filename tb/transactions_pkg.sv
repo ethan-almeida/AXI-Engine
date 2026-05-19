968fa93fa46fb160c2ada5ed657d81f8a68b10e3
ethan-almeida <ethanalmeida184@gmail.com>
2026-05-18 23:59:36 -0600

    removed rand and constraints, using Verilator, no support

--- tb/transactions_pkg.sv
+++ tb/transactions_pkg.sv
@@ -7,9 +7,8 @@ package transactions_pkg;
 
     class transaction_base extends uvm_sequence_item;
         `uvm_object_utils(transaction_base)
-        rand logic [ADDR_WIDTH-1:0] addr;
+        logic [ADDR_WIDTH-1:0] addr;
         logic [1:0] resp;
-        constraint align_addr {addr[1:0] == 2'b00};
 
         function new(string name = "transaction_base");
             super.new(name);
@@ -19,10 +18,9 @@ package transactions_pkg;
 
     class write_trans extends transaction_base;
         `uvm_object_utils(write_trans)
-        rand logic [DATA_WIDTH-1:0] data;
-        rand logic [3:0] strb;
+        logic [DATA_WIDTH-1:0] data;
+        logic [3:0] strb;
 
-        constraint valid_strb { strb inside {4'b0001, 4'b0011, 4'b0111, 4'b1111}};
 
         function new(string name = "write_trans");
             super.new(name);
@@ -31,7 +29,7 @@ package transactions_pkg;
 
     class read_trans extends transaction_base;
         `uvm_object_utils(read_trans)
-        rand logic [DATA_WIDTH-1:0] data;
+        logic [DATA_WIDTH-1:0] data;
         
         function new(string name = "read_trans");
             super.new(name);

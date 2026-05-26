13dfe20d688d03437e296f3739b3f648ceafd243
ethan-almeida <ethanalmeida184@gmail.com>
2026-05-25 19:44:43 -0600

    commented out some unused parameters

--- tb/config_pkg.sv
+++ tb/config_pkg.sv
@@ -2,6 +2,10 @@ package config_pkg;
 
     parameter int DATA_WIDTH = 32;
     parameter int ADDR_WIDTH = 32;
-    parameter int DRIV_CYCLES = 1;
+    // parameter int DRIV_CYCLES = 1;
+    // parameter int WRITE_SEQ_TRANS = 1;
+    // parameter int READ_SEQ_TRANS = 1;
+    parameter int WRITE_GEN_TRANS = 1;
+    parameter int READ_GEN_TRANS = 1;
 
 endpackage

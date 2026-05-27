c1fce9dad53112e12e0130db2e0a7fc6ee9813cc
ethan-almeida <ethanalmeida184@gmail.com>
2026-05-27 11:52:00 -0600

    added ap declarations in config pkg

--- tb/config_pkg.sv
+++ tb/config_pkg.sv
@@ -1,11 +1,15 @@
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+
+`uvm_analysis_imp_decl(_w)
+`uvm_analysis_imp_decl(_r)
+`uvm_analysis_imp_decl(_expected)
+
 package config_pkg;
 
     parameter int DATA_WIDTH = 32;
     parameter int ADDR_WIDTH = 32;
-    // parameter int DRIV_CYCLES = 1;
-    // parameter int WRITE_SEQ_TRANS = 1;
-    // parameter int READ_SEQ_TRANS = 1;
-    parameter int WRITE_GEN_TRANS = 1;
-    parameter int READ_GEN_TRANS = 1;
+    parameter int WRITE_SEQ_TRANS = 1;
+    parameter int READ_SEQ_TRANS = 1;
 
 endpackage

717d1c3706c7c0cd3055c1b32842428635de27ca
ethan-almeida <ethanalmeida184@gmail.com>
2026-05-27 11:53:03 -0600

    added define guards to sequencer classes

--- tb/sequencers_pkg.sv
+++ tb/sequencers_pkg.sv
@@ -5,7 +5,11 @@ package sequencers_pkg;
     import config_pkg::*;
     import transactions_pkg::*;
 
+    `ifndef VERILATOR
     class write_sequencer extends uvm_sequencer #(write_trans);
+    `else
+    class write_sequencer extends uvm_sequencer #(write_trans, write_trans);
+    `endif
         `uvm_component_utils(write_sequencer)
 
         function new(string name, uvm_component parent);
@@ -15,9 +19,12 @@ package sequencers_pkg;
     endclass
 
 
+    `ifndef VERILATOR
     class read_sequencer extends uvm_sequencer #(read_trans);
+    `else
+    class read_sequencer extends uvm_sequencer #(read_trans, read_trans);
+    `endif
         `uvm_component_utils(read_sequencer)
-
         function new(string name, uvm_component parent);
             super.new(name, parent);
         endfunction

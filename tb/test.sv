d4669b00bd3283c94788e9e2623706afed62baf7
ethan-almeida <ethanalmeida184@gmail.com>
2026-05-28 15:09:05 -0600

    added a new test: test_multiple_writes

--- tb/test.sv
+++ tb/test.sv
@@ -1,6 +1,7 @@
 import uvm_pkg::*;
 import environment_pkg::*;
 import sequence_pkg::*;
+import config_pkg::*;
 
 class my_test extends uvm_test;
     `uvm_component_utils(my_test)
@@ -14,6 +15,12 @@ class my_test extends uvm_test;
     endtask
 endclass
 
+
+/*
+ tests for basic functionality
+- single write, then reads from said address
+
+*/
 class test_sanity extends uvm_test;
     `uvm_component_utils(test_sanity)
 
@@ -38,5 +45,42 @@ class test_sanity extends uvm_test;
         repeat(100) @(posedge env.w_mon.monitor.clk);
         phase.drop_objection(this);
     endtask
+endclass
 
+
+/*
+    tests for multiple writes
+    - writes to multiple addresses and then read them back in different order
+
+*/
+class test_multiple_writes extends uvm_test;
+    `uvm_component_utils(test_multiple_writes)
+
+    environment env;
+    write_multiple_seq w_seq;
+    read_multiple_seq r_seq;
+
+    function new(string name = "test_multiple_writes", uvm_component parent = null);
+        super.new(name, parent);
+    endfunction
+
+    function void build_phase(uvm_phase phase);
+        env = environment::type_id::create("env", this);
+        w_seq = write_multiple_seq::type_id::create("w_seq");
+        r_seq = read_multiple_seq::type_id::create("r_seq");
+    endfunction
+
+    virtual task run_phase(uvm_phase phase);
+        phase.raise_objection(this);
+        w_seq.num_writes = MULTIPLE_WRITE_TRANS;
+        w_seq.base_addr  = MULTIPLE_WRITE_BASE_ADDR;
+        w_seq.start(env.w_seqr);
+        
+        r_seq.addr = new[MULTIPLE_WRITE_TRANS];
+        foreach (r_seq.addr[i]) r_seq.addr[i] = $urandom_range(0, 15) * 4; //can change if needed
+        r_seq.start(env.r_seqr);
+        
+        repeat(100) @(posedge env.w_mon.monitor.clk);
+        phase.drop_objection(this);
+    endtask
 endclass
